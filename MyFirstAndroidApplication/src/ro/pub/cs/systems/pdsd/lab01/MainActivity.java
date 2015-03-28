fece9888328ad387c9566864998fe202e4e5c29d
Andreea Stoican <stoican_and@yahoo.com>
2015-03-28 20:11:11 +0200

    L01

--- MyFirstAndroidApplication/src/ro/pub/cs/systems/pdsd/lab01/MainActivity.java
+++ MyFirstAndroidApplication/src/ro/pub/cs/systems/pdsd/lab01/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +28,23 @@ public class MainActivity extends Activity {
 			EditText userNameEditText = (EditText)findViewById(R.id.user_name_edit_text);
 			TextView greetingTextView = (TextView)findViewById(R.id.greeting_text_view);
 			greetingTextView.setAlpha(1);
+			//greetingTextView.setText("Weelcome " + userNameEditText.getText().toString());
+			greetingTextView.setText(greetingTextView.getText().toString().replace("xxx", userNameEditText.getText().toString()));
+			
+			 Animation fadeIn = new AlphaAnimation(0, 1);
+		    fadeIn.setInterpolator(new DecelerateInterpolator()); // add this
+		    fadeIn.setDuration(TRANSPARENCY_EFFECT_DURATION);
+
+		    Animation fadeOut = new AlphaAnimation(1, 0);
+		    fadeOut.setInterpolator(new AccelerateInterpolator()); // and this
+		    fadeOut.setStartOffset(TRANSPARENCY_EFFECT_DURATION + TRANSPARENCY_EFFECT_DURATION);
+		    fadeOut.setDuration(TRANSPARENCY_EFFECT_DURATION);
+
+		    AnimationSet animation = new AnimationSet(false); // change to false
+		    animation.addAnimation(fadeIn);
+		    animation.addAnimation(fadeOut);
+		    animation.setRepeatCount(1);
+		    greetingTextView.setAnimation(animation);
 		}
 		
 	}
